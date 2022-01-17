5babe5fb5e00df71a39f243a9b15581bbb775966
Alex Espinoza <alex_1997_4@hotmail.com>
2022-01-16 22:46:21 -0500

    cambios 1601

--- access.php
+++ access.php
@@ -5,15 +5,16 @@ require "cnSql.php";
 $user=$_POST['user'];
 $pass=$_POST['pass'];
 $rol=0;
-$sql="select * from usuario WHERE usuario='$user' AND password='$pass'";
+$sql="select * from usuario WHERE usuario='$user' AND password='$pass' and deleted is null";
 $query = runSQL($sql);
 $query=json_decode($query);
-
+$res=0;
 foreach($query as $row){
 	$res=1;
 	session_start();
 	$_SESSION['usuario'] = $row->usuario; 
 	$_SESSION['id_user'] = $row->idusuario; 
+	$_SESSION['idperfil'] = $row->idperfil; 
  }  
 }else{
 	//$res="Acceso Prohibido";
